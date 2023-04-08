a74fac896013274ecafab2cde14d999ee82a6936
Minh Do <minh7a12002@gmail.com>
2023-04-08 16:05:15 +0700

    minh - navbar, sidebar

--- bookstore/view/home.php
+++ bookstore/view/home.php
@@ -1,35 +1,18 @@
 <?php
   require_once("./view/header.php");
 ?>
-<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" stl style="margin-bottom: 50px;">
-  <div class="carousel-inner" height = 90%>
-    <div class="carousel-item active" data-bs-interval="2000">
-      <img src="/bookstore/asset/img/banner3.jpg" class="d-block w-100" alt="...">
+<div class="row">
+    <div class="col-sm-2">
+        <?php
+        require_once("./view/sidebar.php");
+        ?>
     </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img src="/bookstore/asset/img/banner2.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="/bookstore/asset/img/banner1.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="/bookstore/asset/img/banner4.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-<div class="container">
-  <h1>Best Seller</h1>
-  <div class="container text-center" data-aos="fade-up">
-    <div class="row align-items-start">
-    <?php 
+    <div class="col-sm-10">
+        <div class="container">
+            <h1 class="my-5">Đối tác tiêu biểu</h1>
+            <div class="container text-center" data-aos="fade-up">
+                <div class="row align-items-start">
+                    <?php 
       if ($data['data']->num_rows > 0) {
         while ($row = $data['data']->fetch_assoc()) {
           $id = $row['id'];
@@ -50,8 +33,10 @@
       }
       else {echo "No Data.";}
     ?>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 
---
+++ bookstore/view/sidebar.php
@@ -0,0 +1,47 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <title>Document</title>
+</head>
+<body> -->
+<nav>
+    <ul>
+        <li>
+            <a href="#" class="logo">
+                <img src="/logo.jpg" alt="">
+                <span class="nav-item">Code Info</span>
+            </a>
+        </li>
+        <li><a href="#">
+                <i class="fas fa-home"></i>
+                <span class="nav-item">Home</span>
+            </a></li>
+        <li><a href="#">
+                <i class="fas fa-user"></i>
+                <span class="nav-item">Home</span>
+            </a></li>
+        <li><a href="#">
+                <i class="fas fa-wallet"></i>
+                <span class="nav-item">Home</span>
+            </a></li>
+        <li><a href="#">
+                <i class="fas fa-wallet"></i>
+                <span class="nav-item">Home</span>
+            </a></li>
+        <li><a href="#">
+                <i class="fas fa-wallet"></i>
+                <span class="nav-item">Home</span>
+            </a></li>
+        <li><a href="#" class="logout">
+                <i class="fas fa-sign-out-alt"></i>
+                <span class="nav-item">Log out</span>
+            </a></li>
+    </ul>
+</nav>
+<!-- </body>
+</html> -->
