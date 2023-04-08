c9cd19d61f1de4e080910409ea2fdc1609db9416
minhlang102 <minhlangstudy@gmail.com>
2023-04-09 05:42:53 +0700

    lang social

--- bookstore/view/header.php
+++ bookstore/view/header.php
@@ -13,6 +13,7 @@
         <link rel="stylesheet" type="text/css" href="/bookstore/asset/sidebar.css" />
         <link rel="stylesheet" type="text/css" href="/bookstore/asset/home.css" />
         <link rel="stylesheet" type="text/css" href="/bookstore/asset/footer.css" />
+        <link rel="stylesheet" type="text/css" href="/bookstore/asset/social.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <title>BloodBond</title>
     </head>
