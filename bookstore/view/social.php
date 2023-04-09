50c7fd4818895fefefed810678d61d93ed59a529
minhlang102 <minhlangstudy@gmail.com>
2023-04-09 12:22:38 +0700

    final

--- bookstore/view/social.php
+++ bookstore/view/social.php
@@ -294,6 +294,74 @@
                 </div><!-- container -->
             </section>
 
+            <section class="card profile-feed shadow-sm">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-10 offset-lg-1">
+
+                            <div class="cardbox bg-white">
+
+                                <div class="cardbox-heading">
+                                    <!-- START dropdown-->
+                                    <div class="dropdown float-right">
+                                        <button class="btn btn-flat btn-flat-icon" type="button" data-toggle="dropdown" aria-expanded="false">
+                                            <em class="fa fa-ellipsis-h"></em>
+                                        </button>
+                                        <div class="dropdown-menu dropdown-scale dropdown-menu-right" role="menu" style="position: absolute; transform: translate3d(-136px, 28px, 0px); top: 0px; left: 0px; will-change: transform;">
+                                            <a class="dropdown-item" href="#">Hide post</a>
+                                            <a class="dropdown-item" href="#">Stop following</a>
+                                            <a class="dropdown-item" href="#">Report</a>
+                                        </div>
+                                    </div><!-- dropdown -->
+                                    <div class="media m-0">
+                                        <div class="d-flex mr-3">
+                                            <a href=""><img class="img-fluid rounded-circle" src="https://nhsbtdbe.blob.core.windows.net/umbraco-assets-corp/15078/david-watson.jpg" alt="User"></a>
+                                        </div>
+                                        <div class="media-body">
+                                            <p class="m-0">H. Amanda</p>
+                                            <small><span><i class="icon ion-md-pin"></i> France</span></small>
+                                            <small><span><i class="icon ion-md-time"></i> 1 hour ago</span></small>
+                                        </div>
+                                    </div><!-- media -->
+                                </div><!-- cardbox-heading -->
+
+                                <div class="cardbox-item">
+                                    <img class="img-fluid" src="https://nhsbtdbe.blob.core.windows.net/umbraco-assets-corp/15078/david-watson.jpg" alt="Image">
+                                </div><!-- cardbox-item -->
+                                <div class="cardbox-base">
+                                    <ul>
+                                        <li><a><i class="fa fa-thumbs-up"></i></a></li>
+                                        <li><a href="#"><img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg" class="img-fluid rounded-circle" alt="User"></a></li>
+                                        <li><a href="#"><img src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg" class="img-fluid rounded-circle" alt="User"></a></li>
+                                        <li><a href="#"><img src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg" class="img-fluid rounded-circle" alt="User"></a></li>
+                                        <li><a href="#"><img src="https://images.pexels.com/photos/6962108/pexels-photo-6962108.jpeg" class="img-fluid rounded-circle" alt="User"></a></li>
+                                        <li><a><span>242 Likes</span></a></li>
+                                    </ul>
+                                    <ul class="float-left">
+                                        <li><a><i class="fa fa-comments"></i></a></li>
+                                        <li><a><em class="mr-5">100</em></a></li>
+                                        <li><a><i class="fa fa-share-alt" style="padding-left: 20px"></i></a></li>
+                                        <li><a><em class="mr-3">10</em></a></li>
+                                    </ul>
+                                </div><!-- cardbox-base -->
+                                <div class="cardbox-comments">
+                                    <span class="comment-avatar float-left">
+                                        <a href=""><img class="rounded-circle" src="https://nhsbtdbe.blob.core.windows.net/umbraco-assets-corp/15078/david-watson.jpg" alt="..."></a>
+                                    </span>
+                                    <div class="search">
+                                        <input placeholder="Write a comment" type="text">
+                                        <button><i class="fa fa-camera"></i></button>
+                                    </div><!-- Search -->
+                                </div><!-- cardbox-like -->
+
+                            </div><!-- cardbox -->
+
+                        </div><!-- col-lg-6 -->
+
+                    </div><!-- row -->
+                </div><!-- container -->
+            </section> 
+
         </div>
     </div>
 
